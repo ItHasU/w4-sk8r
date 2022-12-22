74bc4bc39ee479c02fba74424a4d7caf38c550de
Matthieu Aubry <ithasu@gmail.com>
2022-12-22 12:20:37 +0000

    Logo

--- src/splashscreen.ts
+++ src/splashscreen.ts
@@ -1,3 +1,4 @@
+import * as splash from "./assets/splash";
 import { AbstractFrameHandler, Mode } from "./frameHandler";
 import * as w4 from "./wasm4";
 
@@ -8,15 +9,18 @@ export class SplashScreen extends AbstractFrameHandler {
     }
 
     protected _update_impl(): Mode {
-        store<u16>(w4.DRAW_COLORS, 2);
-        w4.text("Sk8r!", 10, 10);
-
         const gamepad = load<u8>(w4.GAMEPAD1);
         if (gamepad & w4.BUTTON_1) {
             return Mode.PLAY;
         }
 
-        w4.text("Press X to play", 16, 90);
+        store<u16>(w4.DRAW_COLORS, 0x2341);
+        w4.blit(splash.splash, 0, 0, splash.splashWidth, splash.splashHeight, splash.splashFlags);
+
+        store<u16>(w4.DRAW_COLORS, 0x4);
+        w4.text("Press X to play", 17, 141);
+        store<u16>(w4.DRAW_COLORS, 0x1);
+        w4.text("Press X to play", 16, 140);
         return Mode.KEEP;
     }
 }
